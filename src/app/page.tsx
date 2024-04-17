947563064e3e44ae421d36be62341ad422d860fa
Alejandro Rodriguez <alejandro.lr.developer@hotmail.com>
2024-04-17 13:22:28 -0500

    Fix: model to images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,28 +2,17 @@ import { db } from "~/server/db";
 
 export const dynamic = "force-dynamic";
 
-const mockUrls = [
-  "https://utfs.io/f/9a4abed4-c979-4873-aefa-5e313e293d44-uyp24f.png",
-  "https://utfs.io/f/763f9815-42c0-439c-9cd5-f22962e2d01f-278b.jpg",
-];
-
-const mockImages = mockUrls.map((url, idx) => ({
-  id: idx + 1,
-  url,
-}));
-
 export default async function HomePage() {
-  const posts = await db.query.posts.findMany();
+  const images = await db.query.images.findMany({
+    orderBy: (model, { desc }) => desc(model.id),
+  });
 
   return (
     <section>
-      <div className="flex flex-wrap gap-4">
-        {posts.map((p) => (
-          <div key={p.id}>{p.name}</div>
-        ))}
-        {mockImages.map((image) => (
-          <article className="w-48" key={image.id}>
-            <img src={image.url} alt="" />
+      <div className="flex flex-wrap">
+        {images.map((image) => (
+          <article key={image.id}>
+            <img className="w-60" src={image.url} alt={image.name} />
           </article>
         ))}
       </div>
