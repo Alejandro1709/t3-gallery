0c73d227c7b698b405c36ab81d6a5f4e95df7363
Alejandro Rodriguez <alejandro.lr.developer@hotmail.com>
2024-04-17 12:36:05 -0500

    Add: Mock images

---
+++ src/app/components/Navbar.tsx
@@ -0,0 +1,8 @@
+export default function Navbar() {
+  return (
+    <nav className="flex w-full items-center justify-between border-b p-4 text-xl font-semibold">
+      <div>Gallery</div>
+      <div>Sign In</div>
+    </nav>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,23 @@
+const mockUrls = [
+  "https://utfs.io/f/9a4abed4-c979-4873-aefa-5e313e293d44-uyp24f.png",
+  "https://utfs.io/f/763f9815-42c0-439c-9cd5-f22962e2d01f-278b.jpg",
+];
+
+const mockImages = mockUrls.map((url, idx) => ({
+  id: idx + 1,
+  url,
+}));
+
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center">
-      <h1>Hello World</h1>
-    </main>
+    <section>
+      <div className="flex flex-wrap gap-4">
+        {mockImages.map((image) => (
+          <article className="w-48" key={image.id}>
+            <img src={image.url} alt="" />
+          </article>
+        ))}
+      </div>
+    </section>
   );
 }
