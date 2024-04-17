bacce7f67a234d70da5f1e959eab8543742f1283
Alejandro Rodriguez <alejandro.lr.developer@hotmail.com>
2024-04-17 13:02:47 -0500

    Database Setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,5 @@
+import { db } from "~/server/db";
+
 const mockUrls = [
   "https://utfs.io/f/9a4abed4-c979-4873-aefa-5e313e293d44-uyp24f.png",
   "https://utfs.io/f/763f9815-42c0-439c-9cd5-f22962e2d01f-278b.jpg",
@@ -8,10 +10,15 @@ const mockImages = mockUrls.map((url, idx) => ({
   url,
 }));
 
-export default function HomePage() {
+export default async function HomePage() {
+  const posts = await db.query.posts.findMany();
+
   return (
     <section>
       <div className="flex flex-wrap gap-4">
+        {posts.map((p) => (
+          <div key={p.id}>{p.name}</div>
+        ))}
         {mockImages.map((image) => (
           <article className="w-48" key={image.id}>
             <img src={image.url} alt="" />
